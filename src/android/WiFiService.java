9cce34f1df4863ea6e4bf5caa4c3b07cb795b8df
Oliver Schwartzer <ojsglobal@gmail.com>
2015-07-29 09:30:11 +0100

    First Upload

---
+++ src/android/WiFiService.java
@@ -0,0 +1,177 @@
+package com.schwartzer.bgs.wifisniff;
+
+import com.schwartzer.bgs.wifisniff.MainActivity;
+import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
+import com.schwartzer.bgs.wifisniff.R;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import android.support.v4.app.NotificationCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.List;
+
+public class WiFiService extends BackgroundService {
+    private static final String TAG = "WiFiService";
+    private static final int NOTIFICATION_ID = 7124;
+
+    private WifiManager wifi;
+    private String wifiSSID;
+    private boolean listening;
+    private long workingTimestamp, delay;
+
+    private Notification notification;
+    private String notificationTitle, notificationText;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        notificationText = "WiFi Network Detected";
+        notificationTitle = "WiFi Service";
+        notification = null;
+        wifiSSID = "NONE";
+        listening = false;
+        delay = 0;
+        workingTimestamp = 0;
+
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (!listening) {
+                    cancelNotification();
+                    return;
+                }
+
+                // Do not detect anything before delay time passes
+                if ((new Date()).getTime() < workingTimestamp)
+                    return;
+
+                List<ScanResult> results = wifi.getScanResults();
+                boolean networkDetected = false;
+                for (int i = 0; i < results.size(); i++) {
+                    Log.d("WIFIDSSID", results.get(i).SSID + " = " + wifiSSID);
+                    if (results.get(i).SSID.toUpperCase().equals(wifiSSID.toUpperCase())) {
+                        workingTimestamp = (new Date()).getTime() + delay;
+                        showNotification();
+                        networkDetected = true;
+                    }
+                }
+                if (!networkDetected)
+                    cancelNotification();
+            }
+        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+    }
+
+    private void showPersistentNotification() {
+        Intent intent = new Intent(this.getBaseContext(), MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(getBaseContext(), 0, intent, 0);
+        NotificationCompat.Builder nBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.service)
+                        .setContentTitle(notificationTitle)
+                        .setContentText("Tap to open application")
+                        .setOngoing(true)
+                        .setContentIntent(pIntent);
+
+        NotificationManager manager = (NotificationManager)getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID + 1, nBuilder.build());
+    }
+
+    private void cancelPersistentNotification() {
+        NotificationManager manager = (NotificationManager)getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancel(NOTIFICATION_ID + 1);
+        Log.d("WIFIDNOTIFICATION", "Cancelled");
+    }
+
+    private void showNotification() {
+        Intent intent = new Intent(this.getBaseContext(), MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(getBaseContext(), 0, intent, 0);
+        NotificationCompat.Builder nBuilder =
+                new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.service_icon)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationText)
+                .setAutoCancel(true)
+                .setContentIntent(pIntent);
+
+        NotificationManager manager = (NotificationManager)getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID, nBuilder.build());
+    }
+
+    private void cancelNotification() {
+        NotificationManager manager = (NotificationManager)getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancel(NOTIFICATION_ID);
+        Log.d("WIFIDNOTIFICATION", "Cancelled");
+    }
+
+    @Override
+    protected JSONObject initialiseLatestResult() {
+        return null;
+    }
+
+    @Override
+    protected JSONObject doWork() {
+        if(!listening)
+            return null;
+        if(wifi.isWifiEnabled())
+            wifi.startScan();
+
+        return null;
+    }
+
+    @Override
+    protected JSONObject getConfig() {
+        JSONObject result = new JSONObject();
+
+        try {
+            result.put("ssid", wifiSSID);
+        } catch(JSONException _e) {
+            return null;
+        }
+
+        return result;
+    }
+
+    @Override
+    protected void setConfig(JSONObject config) {
+        try {
+            for(int i=0; i<config.names().length(); i++)
+                Log.d("WIFIDCONFIG", config.names().getString(i) + ": " + config.getString(config.names().getString(i)));
+            if(config.has("ssid"))
+                wifiSSID = config.getString("ssid");
+            if(config.has("cancel"))
+                cancelNotification();
+            if(config.has("listening")) {
+                listening = (config.getInt("listening") == 0 ? false : true);
+                workingTimestamp = (new Date()).getTime();
+                if(listening) {
+                    showPersistentNotification();
+                } else {
+                    cancelNotification();
+                    cancelPersistentNotification();
+                }
+            }
+            if(config.has("delay"))
+                delay = config.getInt("delay") * 60000;
+            if(config.has("message"))
+                notificationText = config.getString("message");
+        } catch(JSONException _e) {
+            wifiSSID = null;
+        }
+    }
+}
