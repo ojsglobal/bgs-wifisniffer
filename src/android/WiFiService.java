8949029e660a3aa95524393dd4093d3dd268c37f
Oliver Schwartzer <ojsglobal@gmail.com>
2015-07-29 13:20:01 +0100

    Testing update

--- src/android/WiFiService.java
+++ src/android/WiFiService.java
@@ -5,10 +5,12 @@ import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
 //import com.schwartzer.bgs.wifisniff.R;
 
 
-/*
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+
+/*
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
