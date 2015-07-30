eabca66a9b49e4e0de25444f89f0ba34ea4a82ab
ojsglobal <ojsglobal@users.noreply.github.com>
2015-07-30 11:39:19 +0100

    Icon changes.

--- src/android/WiFiService.java
+++ src/android/WiFiService.java
@@ -90,7 +90,7 @@ public class WiFiService extends BackgroundService {
         PendingIntent pIntent = PendingIntent.getActivity(getBaseContext(), 0, intent, 0);
         NotificationCompat.Builder nBuilder =
                 new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.divider_horizontal_dark_opaque)
+                        .setSmallIcon(R.drawable.arrow_down_float)
                         .setContentTitle(notificationTitle)
                         .setContentText("Working correctly.")
                         .setOngoing(true)
@@ -113,7 +113,7 @@ public class WiFiService extends BackgroundService {
         PendingIntent pIntent = PendingIntent.getActivity(getBaseContext(), 0, intent, 0);
         NotificationCompat.Builder nBuilder =
                 new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_popup_reminder)
+                .setSmallIcon(R.drawable.arrow_down_float)
                 .setContentTitle(notificationTitle)
                 .setContentText(notificationText)
                 .setAutoCancel(true)
